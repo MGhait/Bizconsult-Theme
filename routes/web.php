78a83f84fb1c684ffbae5f260a7cde852e64cb0b
Mohamed Saad <mosaad05019@gmail.com>
2024-10-13 17:47:55 +0300

    admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('front.index');
+Route::name('front.')->group(function(){
+    Route::view('/','front.index')->name('index');
+    Route::view('/about','front.about')->name('about');
+    Route::view('/service','front.service')->name('service');
+    Route::view('/contact','front.contact')->name('contact');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// there is a problem naming the route with admin
+Route::name('admin.')->prefix('myAdmin')->group(function(){
+    Route::view('/','admin.index')->name('index');
+    Route::view('/login','admin.auth.login')->name('login');
 });
 
-require __DIR__.'/auth.php';
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
